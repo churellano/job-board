16a838aca5db53717bfcc6a90249d4ada78f6530
jeremiah <churellano@gmail.com>
2020-03-03 12:51:08 -0800

    Make navbar clickable and add more basic styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 class Navbar extends Component {
+
   render() {
     return (
       <nav className='navbar'>
           <h3> Logo </h3>
-          <ul className='navbar-links'>
+          <ul className='navbar-list'>
+            <Link className='navbar-links' to='/'>
               <li>Home</li>
+            </Link>
+            <Link className='navbar-links' to='/profile'>
               <li>Profile</li>
+            </Link>
+            <Link className='navbar-links' to='/listings'>
               <li>Listings</li>
+            </Link> 
           </ul>
       </nav>
     );
