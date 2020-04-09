849f6874635df6848ea313bb15a9fac37bdbd7f3
churellano <churellano@gmail.com>
2020-04-09 01:29:55 -0700

    Change equality to type check

--- client/src/components/Listings/JobPreview.jsx
+++ client/src/components/Listings/JobPreview.jsx
@@ -13,31 +13,31 @@ class JobPreview extends Component {
     let deadlineText = (daysLeft) => {
       if (daysLeft > 1) {
         return String(daysLeft + ' days left');
-      } else if (daysLeft == 1) {
+      } else if (daysLeft === 1) {
         return String(daysLeft + ' day left');
-      } else if (daysLeft == 0) {
+      } else if (daysLeft === 0) {
         return 'Deadline today';
       }
     }
     return (
       <Paper variant='outlined'>
-      <div className='job-preview'>
-        <div className='job-summary'>
-          <div className='job-info'>
-            <h3>{this.props.data.position}</h3>
-            <p>{this.props.data.organization}</p>
-            <p>{this.props.data.location}</p>
-            <p>{this.props.data.duration.join(', ')} months</p>
+        <div className='job-preview'>
+          <div className='job-summary'>
+            <div className='job-info'>
+              <h3>{this.props.data.position}</h3>
+              <p>{this.props.data.organization}</p>
+              <p>{this.props.data.location}</p>
+              <p>{this.props.data.duration.join(', ')} months</p>
+            </div>
+            <div className='job-deadline'>
+              <p>{deadlineText(daysLeft)}</p>
+            </div>
           </div>
-          <div className='job-deadline'>
-            <p>{deadlineText(daysLeft)}</p>
+          <div className='job-action-list'>
+            <button> Apply </button>
+            <button> Favourite </button>
           </div>
         </div>
-        <div className='job-action-list'>
-          <button> Apply </button>
-          <button> Favourite </button>
-        </div>
-      </div>
       </Paper>
     );
   }
