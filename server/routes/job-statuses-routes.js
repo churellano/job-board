6ca9c6fabe4f2ad4ac37ccf9793737abae52bed5
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-10-20 00:34:53 -0700

    Add routes file for job-statuses

--- server/routes/job-statuses-routes.js
+++ server/routes/job-statuses-routes.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
 const jobStatusesDb = require('../queries/job-statuses-queries');
 
 /* GET statuses listing. */
-router.get('/', jobStatusesDb.getJobStatuses);
+router.get('/', 
+  passport.authenticate('jwt', {session: false}), (req, res) => {
+  const { user } = req;
+  jobStatusesDb.getJobStatuses();
+
+  res.status(200).send({ user });
+});
 
 /* GET status by id */
 router.get('/:id', jobStatusesDb.getJobStatusById);
