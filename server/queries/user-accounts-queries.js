d1a1fe1346533a44d33a1a167f10b6e977023ddb
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-10-20 00:34:32 -0700

    Add routes to get useraccounts by username or id

--- server/queries/user-accounts-queries.js
+++ server/queries/user-accounts-queries.js
@@ -1,51 +1,126 @@
 "use strict";
 
+const Roles = {
+  Administrator: 1,
+  Student: 2,
+  Employer: 3
+}
+
 const pool = require('../services/dbConnectService');
 
-const getUserAccounts = (request, response) => {
-  pool.connect().then(client => {
-    client.query('SELECT * FROM UserAccount ORDER BY id ASC').then(result => {
-      client.release();
-      response.status(200).json(result.rows);
-    })
-    .catch(error => {
-      client.release();
-      console.log(error.stack);
-    });
-  });
+const get = async (callback) => {
+  let client;
+  try {
+    client = await pool.connect();
+    const userAccountsQuery = await client.query('SELECT * FROM UserAccount ORDER BY id ASC');
+    // console.log('here2', userAccountsQuery.rows);
+    callback(null, userAccountsQuery.rows);
+  } catch (error) {
+    console.log('here');
+    callback(error, null);
+  } finally {
+    client.release();
+  }
 }
 
-const getUserAccountById = (id, callback) => {
-  pool.connect().then(client => {
-    client.query('SELECT * FROM UserAccount WHERE id = $1', [id]).then(result => {
-      client.release();
-      callback(null, result.rows[0]);
-    })
-    .catch(error => {
-      client.release();
-      console.log(error.stack);
-    });
-  });
+const getUserAccountById = async (id, callback) => {
+  let client;
+  try {
+    client = await pool.connect();
+    const userAccountQuery = await client.query('SELECT * FROM UserAccount WHERE Id = $1', [id]);
+    console.log('here', userAccountQuery.rows);
+    callback(null, userAccountQuery.rows);
+  } catch (error) {
+    console.log('here2');
+    callback(error, null);
+  } finally {
+    console.log('here3');
+    client.release();
+  }
 }
 
-const getUserAccountByUsername = (username, callback) => {
+const getUserAccountByUsername = async (username, callback) => {
   console.log('getUserAccountByUserName PARAMS', username);
 
-  pool.connect().then(client => {
-    client.query('SELECT * FROM UserAccount WHERE Username = $1', [username]).then(result => {
-      client.release();
-      callback(null, result.rows[0]);
-    })
-    .catch(error => {
-      client.release();
-      console.log(error.stack);
-    });
-  });
+  let client;
+  try {
+    client = await pool.connect();
+    const userAccountQuery = await client.query('SELECT * FROM UserAccount WHERE Username = $1', [username]);
+    console.log('here', userAccountQuery.rows);
+    callback(null, userAccountQuery.rows);
+  } catch (error) {
+    console.log('here2');
+    callback(error, null);
+  } finally {
+    console.log('here3');
+    client.release();
+  }
 }
 
+const post = async (newUserAccount, callback) => {
+  console.log('post PARAMS', newUserAccount);
+  const { roleId, username, password, firstName, lastName, contactEmail, contactPhone } = newUserAccount;
+  
+  let client;
+  try {
+    client = await pool.connect();
+  } catch (error) {
+    callback(error, null);
+  }
+
+  console.log(parseInt(roleId) === Roles.Student);
+  if (parseInt(roleId) === Roles.Student) {
+    console.log('Attempting to INSERT a new student UserAccount');
+
+    try {
+      // Create entry in UserAccount
+      const insertUserAccount = await client.query(
+        'INSERT INTO UserAccount (RoleId, Username, Password, FirstName, LastName, ContactEmail, ContactPhone) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+        [roleId, username, password, firstName, lastName, contactEmail, contactPhone]
+      );
+        
+      // Create entry in Student with the Foreign key on UserAccountId
+      await client.query(
+        'INSERT INTO Student (UserAccountId, EmploymentStatusId) SELECT Id, 1 FROM UserAccount WHERE Username = ($1)', 
+        [username]
+      );
+
+      console.log('release on success student');
+      client.release();
+      callback(null, insertUserAccount);
+
+    } catch (error) {
+      console.log('release on failure student');
+      client.release();
+      console.error(error.stack);
+      callback(error, null);
+    }
+  } else if (parseInt(roleId) === Roles.Employer) {
+
+    try {
+      // Create entry in UserAccount
+      const insertUserAccount = await client.query(
+        'INSERT INTO UserAccount (RoleId, Username, Password, FirstName, LastName, ContactEmail, ContactPhone) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+        [roleId, username, password, firstName, lastName, contactEmail, contactPhone]
+      );
+
+      console.log('release on success employer/recruiter');
+      client.release();
+      callback(insertUserAccount);
+
+    } catch (error) {
+      console.log('release on failure employer/recruiter');
+      client.release();
+      console.error(error.stack);
+      callback(error, null);
+    }
+    
+  }
+}
 
 module.exports = {
-  getUserAccounts,
+  get,
   getUserAccountById,
-  getUserAccountByUsername
+  getUserAccountByUsername,
+  post
 }
--- server/routes/user-accounts-routes.js
+++ server/routes/user-accounts-routes.js
@@ -1,15 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+const passport = require('passport');
 const userAccountsDb = require('../queries/user-accounts-queries');
 
 /* GET UserAccounts listing. */
-router.get('/', userAccountsDb.getUserAccounts);
+router.get('/', function(req, res) {
+  console.log(req.params);
+  userAccountsDb.get(function(err, result) {
+    if (err || !result) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
 
 /* GET UserAccount by id */
-router.get('/:id', userAccountsDb.getUserAccountById);
+router.get('/id/:id', function(req, res) {
+  console.log('params', req.id);
+  const id = req.params.id;
+  userAccountsDb.getUserAccountById(id, function(err, result) {
+    if (err || !result) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
 
 /* GET UserAccount by username */
-// router.get('/:id', userAccountsDb.getUserAccountsById);
+router.get('/username/:username', function(req, res) {
+  console.log('params', req.params.username);
+  const username = req.params.username;
+  userAccountsDb.getUserAccountByUsername(username, function(err, result) {
+    if (err || !result) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
 
 module.exports = router;
