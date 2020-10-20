4be6ac91ab2ea68d1a3f3b5dfb447ad99c18b792
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-10-20 00:37:05 -0700

    Test protected route

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { API_URL } from '../../constants';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event) {
+    alert('Submit');
+
+    await fetch(API_URL + 'protected', { credentials: 'include'  })
+      .then(response => response.json())
+      .then(data => console.log(data));
+    // event.preventDefault();
+  }
+ 
   render() {
     return (
       <div className="profile">
         <h1> Student profile </h1>
+        <input type='submit' value="Submit" onClick={this.handleSubmit}/>
       </div>
     );
   }
