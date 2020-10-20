639729060c6543f90ca9216f00557c309d28bf0e
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-10-20 00:36:22 -0700

    Fix cors/cookie issues and move passport authentication to separate file

--- server/app.js
+++ server/app.js
@@ -7,55 +7,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
+// var Strategy = require('passport-local').Strategy;
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-const userAccountsDb = require('./queries/user-accounts-queries');
+// const userAccountsDb = require('./queries/user-accounts-queries');
 
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, done) {
-    userAccountsDb.getUserAccountByUsername(username, function(err, user) {
-      if (err) { 
-        return done(err); 
-      }
-      if (!user) { 
-        return done(null, false, { message: 'Username not found.' });
-      }
-      if (user.password != password) { 
-        return done(null, false, { message: 'Invalid password.' }); 
-      }
-
-      return done(null, user);
-    });
-  }));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, done) {
-  console.log('serializeUser');
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  console.log('deserializeUser', id);
-  userAccountsDb.getUserAccountById(id, function (err, user) {
-    if (err) { 
-      return done(err); 
-    }
-    done(null, user);
-  });
-});
+require('./authentication/passport')(passport);
 
 var app = express();
 
@@ -68,12 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: "cats" }));
+// app.use(session({ secret: "cats" }));
+app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 app.use('/', indexRouter);
 
