5b182bbdf2cb9382d8117d7d401ffb3bf7072b6d
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-11-01 16:30:02 -0800

    Change service to use sequelize instead of pg pools

--- server/services/dbConnectService.js
+++ server/services/dbConnectService.js
@@ -2,20 +2,36 @@
 
 // TODO: Store this somewhere more secure
 const pg = require('pg');
+const { Sequelize } = require('sequelize');
+
+
 const DB_USER = 'postgres';
 const DB_HOST = 'localhost';
 const DB_NAME = 'JobBoard';
 const DB_PASSWORD = 'temppwd';
 const DB_PORT = 5432;
 
-const config = {
-  user: DB_USER,
+// const config = {
+//   user: DB_USER,
+//   host: DB_HOST,
+//   database: DB_NAME,
+//   password: DB_PASSWORD,
+//   port: DB_PORT
+// };
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT
-};
+  port: DB_PORT,
+  dialect: 'postgres'
+});
 
-const connectionPool = new pg.Pool(config);
+try {
+  db.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
-module.exports = connectionPool;
+// const connectionPool = new pg.Pool(config);
+
+module.exports = db;
