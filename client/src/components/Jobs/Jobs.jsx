7197cf127311ac0c74e9ca039fbb4e60f912fe15
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-11-11 13:58:56 -0800

    Clean up job listing styling and add jobdetail to view

--- client/src/components/Jobs/Jobs.jsx
+++ client/src/components/Jobs/Jobs.jsx
@@ -1,33 +1,93 @@
 import React, { Component } from "react";
+import JobDetail from '../JobDetail/JobDetail';
 import { JOBS_API } from '../../constants';
+import './Jobs.css';
 
 export default class Jobs extends Component {
   constructor() {
     super();
     this.state = {
-      jobs: []
+      jobs: [],
+      selectedJob: null
     };
+
+    this.renderLocation = this.renderLocation.bind(this);
   }
 
   componentDidMount() {
     getData(JOBS_API).then(response => {
       console.log(response);
-      this.setState(prevState => ({ jobs: response }));
+      this.setState(prevState => ({ jobs: response, selectedJob: response[0] }));
       console.log(this.state);
     });
   }
 
+  renderLocation(job) {
+    let location = job.City;
+    if (job.Province != null) {
+      location += ', ' + job.Province.Abbreviation;
+    } else if (job.State != null) {
+      location += ', ' + job.State.Abbreviation;
+    } else if (job.Region != null) {
+      location += ', ' + job.Region;
+    }
+
+    location += ', ' + job.Country.Name;
+
+    return location;
+  }
+
+  setSelectedJob(job) {
+    this.setState(prevState => ({ selectedJob: job }));
+  }
+
   render() {
+    let jobTableBody = this.state.jobs.map(job => {
+      return (
+        <tr key={job.Id}>
+          <td>{job.JobStatus.Name}</td>
+          <td>{job.Deadline}</td>
+          <td>{job.Semester.Name + ' ' + job.StartYear} </td>
+          <td>{job.Title}</td>
+          <td>{job.Company.Name}</td>
+          <td>{ this.renderLocation(job) }</td>
+          <td>
+            <button className="button is-link is-small" onClick={() => this.setSelectedJob(job)}>View</button>
+            <button className="button is-small">Favourite</button>
+          </td>
+        </tr>
+      );
+    });
+
+    let props = {
+      selectedJob: this.state.selectedJob,
+      renderLocation: this.renderLocation
+    };
+
     return (
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Company</th>
-          </tr>
-        </thead>
-        {this.state.jobs.map(job => <tr key={job.id}> <td>{job.title}</td> <td>{job.companyid}</td> </tr>)}
-      </table>
+      <div className='jobs-container'>
+        <div className='table-container'>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Status</th>
+                <th>Deadline</th>
+                <th>Start term</th>
+                <th>Title</th>
+                <th>Company</th>
+                <th>Location</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              { jobTableBody }
+            </tbody>
+          </table>
+        </div>
+        
+        { this.state.selectedJob ? <JobDetail {...props} /> : null }
+      </div>
+      
     );
   }
 }
