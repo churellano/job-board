999444527149bb4cc8fe918ce6311f159fa2987d
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2020-11-11 14:00:03 -0800

    Change logo button to redirect to home page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ class Navbar extends Component {
     return (        
       <nav className='navbar' role='navigation' aria-label='main navigation'>
         <div className='navbar-brand'>
-          <a className="navbar-item" href="https://bulma.io">
+          <a className="navbar-item" href="/home">
             <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28"/  >
           </a>
           <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
